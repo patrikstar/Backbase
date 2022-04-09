4b9893868d9c730bcdb9bcecbdee31a044c8bcde
Strashnykh Dmytro <patrikstarman@gmail.com>
2022-04-09 11:31:56 +0200

    add buildSrc folder move to ktx setup gradle files

---
+++ app/build.gradle.kts
@@ -0,0 +1,87 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-parcelize")
+    id("kotlin-kapt")
+}
+
+android {
+    compileSdk = AndroidSdk.compile
+    buildToolsVersion = AndroidSdk.buildTools
+
+    defaultConfig {
+        applicationId = "com.backbase"
+        minSdk = AndroidSdk.min
+        targetSdk = AndroidSdk.target
+        versionCode = AppVersion.code
+        versionName = AppVersion.name
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        testProguardFile("proguard-rules-test.pro")
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isDebuggable = true
+            isJniDebuggable = true
+            isRenderscriptDebuggable = true
+            isMinifyEnabled = false
+            buildConfigField("boolean", "LOG_ENABLE", "true")
+            applicationIdSuffix = ".debug"
+        }
+
+        getByName("release") {
+            isDebuggable = false
+            isJniDebuggable = false
+            isRenderscriptDebuggable = false
+            isMinifyEnabled = true
+            buildConfigField("boolean", "LOG_ENABLE", "true")
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = AndroidSdk.java
+        targetCompatibility = AndroidSdk.java
+    }
+
+    kotlinOptions {
+        jvmTarget = AndroidSdk.jvmTarget
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar", "*.aar"))))
+
+    implementation(Libs.Kotlin.stdlib)
+
+    implementation(Libs.appcompat)
+    implementation(Libs.coreKtx)
+    implementation(Libs.Lifecycle.viewModel)
+    implementation(Libs.Lifecycle.livedata)
+
+    implementation(Libs.material)
+    implementation(Libs.constraintLayout)
+    implementation(Libs.materialDialog)
+
+    implementation(Libs.gson)
+
+    implementation(Libs.coroutines)
+    implementation(Libs.coroutines_core)
+
+    implementation(Libs.koin_android)
+
+    implementation(Libs.timber)
+
+    testImplementation(Libs.Test.junit4)
+    testImplementation(Libs.Test.mockk)
+    testImplementation(Libs.Test.coroutines)
+    androidTestImplementation(Libs.Test.mockk_android)
+    androidTestImplementation(Libs.Test.junit4)
+    androidTestImplementation(Libs.Test.espresso)
+}
